a545824eb781ff49f3cd08165902508563f06cf5
Hoooolly <haoxu815@outlook.com>
2017-05-19 13:08:27 +0000

    Update harris_detektor.m von XUHAo

--- HA1/harris_detektor.m
+++ HA1/harris_detektor.m
@@ -1,4 +1,4 @@
-function  Merkmale = harris_detektor(Image,varargin) 
+function  Merkmale = harris_detektor(Image) 
 % In dieser Funktion soll der Harris-Detektor implementiert werden, der
 % Merkmalspunkte aus dem Bild extrahiert
 
@@ -12,26 +12,23 @@ I(1,:) = I(2,:);
 I(end,:) = I(end-1,:);
 I(:,1) = I(:,2);
 I(:,end) = I(:, end-1);
-
-%%
+[Ix,Iy] = sobel_xy(Image);
+%fmask1= fspecial('gaussian',[15,1],15/5);
+%%　set the valuue of parameters
 k = 0.05;
-tau = 50;
-for xx = 2 : m-1 %this border should be considered again
-    for yy = 2 : n-1
-        for k = 1 : 3
-            for l = 1 : 3
-                i = xx - (3 + 1)/2 + k;
-                j = yy - (3 + 1)/2 + l;
-                Harris_matrix = [(I(i+1,j)-I(i,j))^2, (I(i+1,j)-I(i,j))*(I(i,j+1)-I(i,j));
-                    (I(i+1,j)-I(i,j))*(I(i,j+1)-I(i,j)),(I(i,j+1)-I(i,j))^2 ];
-                [U,S,V] = svd(Harris_matrix);
-                H = det(U) - k * trace(U);
-                if H > tau
-                    Merkmale = [Merkmale;xx, yy];
-                end
-            end
-        end
-    end
-end
+tau = 0.5;
 
+w=fspecial('gaussian',3,3);
+Ixx=conv2(Ix.*Ix,w,'same');
+Ixy=conv2(Ix.*Iy,w,'same');
+Iyy=conv2(Iy.*Iy,w,'same');
+
+size(Ixx)
+
+H = (Ixx .* Iyy - Ixy.^2) - k * (Ixx + Iyy).^2;
+
+[Merkmale(:,1), Merkmale(:,2)] = find(H > tau); 
+
+
+   
 end
