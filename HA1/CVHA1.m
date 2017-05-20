99ee48396f7496cdab4738f7834b460fd1041394
Jiachen XU <jiachen.xu@tum.de>
2017-05-20 15:12:25 +0000

    Update: main function

--- HA1/CVHA1.m
+++ HA1/CVHA1.m
@@ -5,20 +5,25 @@
 %  Einlesen und Konvertieren von Bildern sowie Bestimmung von 
 %  Merkmalen mittels Harris-Detektor. 
 
-%  Für die letztendliche Abgabe bitte die Kommentare in den folgenden Zeilen
-%  enfernen und sicherstellen, dass alle optionalen Parameter über den
-%  entsprechenden Funktionsaufruf fun('var',value) modifiziert werden können.
+%  F?r die letztendliche Abgabe bitte die Kommentare in den folgenden Zeilen
+%  enfernen und sicherstellen, dass alle optionalen Parameter ?ber den
+%  entsprechenden Funktionsaufruf fun('var',value) modifiziert werden k?nnen.
 
-
+clear ;
+clc ;
+close all ;
 %% Bild laden
   Image = imread('szene.jpg');
   IGray = rgb_to_gray(Image);
 
-
-
-
 %% Harris-Merkmale berechnen
   tic;
-  %Merkmale = harris_detektor(IGray,'do_plot',true);
-  Merkmale = harris_detektor(IGray);
+  Merkmale = harris_detektor(IGray,'do_plot',true,3 ,0.05 , 9e7,20 ,5 ,200,200);% L?sung f?r Problem 2.(1)
+
+  Merkmale_advanved = harris_detektor_advanced(IGray,'do_plot',true,3 ,0.05 , 9e5,20 ,5 ,200,200);% L?sung f?r Problem 2.(1)
+
+  % Input argument order: image ,'do_plot', true or false , segment_length , k , tau ,min_dist , N ,tile_size 
+  % Otherwise default setting: image ,'do_plot', true , 3 , 0.05 , 9e5 ,20 , 5 ,200(square) or 200,100(rectangle);
+
+
   toc;
