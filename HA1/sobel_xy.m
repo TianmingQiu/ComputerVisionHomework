936287a8cddfb13d9fea2ed648e4d5baa2317768
Jiachen XU <jiachen.xu@tum.de>
2017-05-20 15:06:12 +0000

    Use convolution to compute

--- HA1/sobel_xy.m
+++ HA1/sobel_xy.m
@@ -1,42 +1,22 @@
-function [Fx,Fy] = sobel_xy(Image)
+function [d_Fx,d_Fy] = sobel_xy(Image)
 % In dieser Funktion soll das Sobel-Filter implementiert werden, welches
 % ein Graustufenbild einliest und den Bildgradienten in x- sowie in
-% y-Richtung zurückgibt.
+% y-Richtung zur?ckgibt.
+
+
+
 
 I = Image;
 %define the sobel mask
 sobel_x = [1 0 -1; 2 0 -2; 1 0 -1];
 sobel_y = [1 2 1; 0 0 0; -1 -2 -1];
 
-%suppose add zeros at the borders
-[m n] = size(I);
-Im_in = zeros(m+2,n+2);
-Im_out = Im_in;
-Im_in(2:end-1,2:end-1) = I;
 
-for i = 2:m-1
-    for j = 2:n-1
-        for k = -1:1
-            for l = -1:1
-                Im_out(i,j) = Im_out(i,j) + Im_in(i+k,j+l) * sobel_x(2-k,2-l);
-            end
-        end
-    end
-end
+Fx1 = conv2(sobel_x, I ) ;
+Fy1 = conv2(sobel_y, I ) ;
 
-Fx = Im_out(2:end-1,2:end-1);
 
-Im_out = 0*Im_out;
-
-for i = 2:m-1
-    for j = 2:n-1
-        for k = -1:1
-            for l = -1:1
-                Im_out(i,j) = Im_out(i,j) + Im_in(i+k,j+l) * sobel_y(2-k,2-l);
-            end
-        end
-    end
-end
-Fy = Im_out(2:end-1,2:end-1);
+Fx = Fx1(2:end-1,2:end-1) ;
+Fy = Fy1(2:end-1,2:end-1) ;
 end
 
