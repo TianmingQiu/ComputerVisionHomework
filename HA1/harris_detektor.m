1979dc6302e3e005e16ec7653db4311004d513a0
Hoooolly <haoxu815@outlook.com>
2017-05-20 15:58:23 +0000

    Update harris_detektor.m

--- HA1/harris_detektor.m
+++ HA1/harris_detektor.m
@@ -2,7 +2,7 @@ function   Merkmale = harris_detektor(Image,varargin)
 % In dieser Funktion soll der Harris-Detektor implementiert werden, der
 % Merkmalspunkte aus dem Bild extrahiert
     
-
+% To judge whether to display the processed image.
     flag = 0 ; 
 
     if (varargin{1} == 'do_plot')
@@ -13,16 +13,16 @@ function   Merkmale = harris_detektor(Image,varargin)
         end;
     end;
 
-
+% Define the global parameter which will be used in the block(tile) processing function 
     global segment_length;
     global k;
     global tau;
 
 
     
-    
+ % Assign default argument or assign the input argument     
     if (nargin == 3)
-        segment_length = 3 ;
+        segment_length = 15 ;
         k = 0.05 ;
         tau = 9e7 ;
         
@@ -45,17 +45,19 @@ function   Merkmale = harris_detektor(Image,varargin)
     I(:,end) = I(:, end-1);
     [Ix,Iy] = sobel_xy(I);
     
-    
+   % Use the Gaussian distribution as weight parameter, 
+   % And also use convolution to get desired G matrix
     w=fspecial('gaussian',segment_length,segment_length);
     Ixx=conv2(Ix.*Ix,w,'same');
     Ixy=conv2(Ix.*Iy,w,'same');
     Iyy=conv2(Iy.*Iy,w,'same');
 
-    
+    % Calculate the Harris matrix 
     H = (Ixx .* Iyy - Ixy.^2) - k * (Ixx + Iyy).^2;
-    
+    % use tau als threshold parameter to judge the ecken.
     [Merkmale(:,1), Merkmale(:,2)] = find(H > tau);
     
+    %if input 'do_plot' & true then set flag = 1, which means to draw the processed image
     if ( flag == 1 )
         figure,
         imshow(Image);
@@ -64,5 +66,4 @@ function   Merkmale = harris_detektor(Image,varargin)
     end;
 
 
-   
 end
