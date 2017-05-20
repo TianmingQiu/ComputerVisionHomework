3b1ff61c060aff298aae06f1a3de0c12858b1bed
Jiachen XU <jiachen.xu@tum.de>
2017-05-20 15:23:46 +0000

    Update sobel_xy.m

--- HA1/sobel_xy.m
+++ HA1/sobel_xy.m
@@ -1,4 +1,4 @@
-function [d_Fx,d_Fy] = sobel_xy(Image)
+function [Fx,Fy] = sobel_xy(Image)
 % In dieser Funktion soll das Sobel-Filter implementiert werden, welches
 % ein Graustufenbild einliest und den Bildgradienten in x- sowie in
 % y-Richtung zur?ckgibt.
