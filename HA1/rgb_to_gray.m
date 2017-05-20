8d2eaccf9fbd7bcdd072b0862f28c833560b69dd
Jiachen XU <jiachen.xu@tum.de>
2017-05-20 15:07:38 +0000

    Update: add the judge sentence when gray image is as input

--- HA1/rgb_to_gray.m
+++ HA1/rgb_to_gray.m
@@ -1,5 +1,7 @@
 function [Gray_image] = rgb_to_gray(Image)
 % Diese Funktion soll ein RGB-Bild in ein Graustufenbild umwandeln. Falls
-% das Bild bereits in Graustufen vorliegt, soll es direkt zurückgegeben werden.
+% das Bild bereits in Graustufen vorliegt, soll es direkt zur?ckgegeben werden.
+
+if 
 Gray_image = 0.299 * Image(:,:,1) + 0.587 * Image(:,:,2) + 0.114 * Image(:,:,3); 
 end
